e1fbaba56acede7910867d83bf2faad04bc80b5f
Алина Рыскова <teekupinmuotti@gmail.com>
2023-09-15 22:54:28 +0300

    docs folder

--- gulp/docs.js
+++ gulp/docs.js
@@ -3,14 +3,12 @@ const gulp = require('gulp');
 // HTML
 const fileInclude = require('gulp-file-include');
 const htmlclean = require('gulp-htmlclean');
-const webpHTML = require('gulp-webp-html');
 
 // SASS
 const sass = require('gulp-sass')(require('sass'));
 const sassGlob = require('gulp-sass-glob');
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
-const webpCss = require('gulp-webp-css');
 
 const server = require('gulp-server-livereload');
 const clean = require('gulp-clean');
@@ -25,7 +23,7 @@ const changed = require('gulp-changed');
 
 // Images
 const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
+// const webp = require('gulp-webp');
 
 
 gulp.task('clean:docs', function (done) {
@@ -42,23 +40,22 @@ const fileIncludeSetting = {
 	basepath: '@file',
 };
 
-const plumberNotify = (title) => {
-	return {
-		errorHandler: notify.onError({
-			title: title,
-			message: 'Error <%= error.message %>',
-			sound: false,
-		}),
-	};
-};
+// const plumberNotify = (title) => {
+// 	return {
+// 		errorHandler: notify.onError({
+// 			title: title,
+// 			message: 'Error <%= error.message %>',
+// 			sound: false,
+// 		}),
+// 	};
+// };
 
 gulp.task('html:docs', function () {
 	return gulp
 		.src(['./src/html/**/*.html', '!./src/html/blocks/**/*.html'])
 		.pipe(changed('./docs/'))
-		.pipe(plumber(plumberNotify('HTML')))
+		// .pipe(plumber(plumberNotify('HTML')))
 		.pipe(fileInclude(fileIncludeSetting))
-		.pipe(webpHTML())
 		.pipe(htmlclean())
 		.pipe(gulp.dest('./docs/'));
 });
@@ -67,11 +64,10 @@ gulp.task('sass:docs', function () {
 	return gulp
 		.src('./src/scss/*.scss')
 		.pipe(changed('./docs/css/'))
-		.pipe(plumber(plumberNotify('SCSS')))
+		// .pipe(plumber(plumberNotify('SCSS')))
 		.pipe(sourceMaps.init())
 		.pipe(autoprefixer())
 		.pipe(sassGlob())
-		.pipe(webpCss())
 		.pipe(groupMedia())
 		.pipe(sass())
 		.pipe(csso())
@@ -83,7 +79,6 @@ gulp.task('images:docs', function () {
 	return gulp
 		.src('./src/img/**/*')
 		.pipe(changed('./docs/img/'))
-		.pipe(webp())
 		.pipe(gulp.dest('./docs/img/'))
 		.pipe(gulp.src('./src/img/**/*'))
 		.pipe(changed('./docs/img/'))
@@ -109,7 +104,7 @@ gulp.task('js:docs', function () {
 	return gulp
 		.src('./src/js/*.js')
 		.pipe(changed('./docs/js/'))
-		.pipe(plumber(plumberNotify('JS')))
+		// .pipe(plumber(plumberNotify('JS')))
 		.pipe(babel())
 		.pipe(webpack(require('./../webpack.config.js')))
 		.pipe(gulp.dest('./docs/js/'));
